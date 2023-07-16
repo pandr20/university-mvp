bb27ddc26de0d25776f0a4c2cd731917b055d130
pandr20 <71690281+pandr20@users.noreply.github.com>
2023-07-16 19:10:25 +0200

    Update actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,12 +1,27 @@
 import { GraphQLClient } from "graphql-request";
 
-import { createProjectMutation, createUserMutation, deleteProjectMutation, updateProjectMutation, getProjectByIdQuery, getProjectsOfUserQuery, getUserQuery, projectsQuery } from "@/graphql";
+import {
+  createProjectMutation,
+  createUserMutation,
+  deleteProjectMutation,
+  updateProjectMutation,
+  getProjectByIdQuery,
+  getProjectsOfUserQuery,
+  getUserQuery,
+  projectsQuery,
+} from "@/graphql";
 import { ProjectForm } from "@/common.types";
 
-const isProduction = process.env.NODE_ENV === 'production';
-const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
-const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
-const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+  : "http://127.0.0.1:4000/graphql";
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  : "letmein";
+const serverUrl = isProduction
+  ? process.env.NEXT_PUBLIC_SERVER_URL
+  : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
 
@@ -41,33 +56,47 @@ const makeGraphQLRequest = async (query: string, variables = {}) => {
   }
 };
 
-export const fetchAllProjects = (category?: string | null, endcursor?: string | null) => {
+export const fetchAllProjects = (
+  category: string | null = null,
+  endcursor: string | null = null
+) => {
   client.setHeader("x-api-key", apiKey);
 
-  return makeGraphQLRequest(projectsQuery, { category, endcursor });
+  return makeGraphQLRequest(projectsQuery, {
+    category: category || "", // Set category to an empty string if it's null
+    endcursor,
+  });
 };
 
-export const createNewProject = async (form: ProjectForm, creatorId: string, token: string) => {
+export const createNewProject = async (
+  form: ProjectForm,
+  creatorId: string,
+  token: string
+) => {
   const imageUrl = await uploadImage(form.image);
 
   if (imageUrl.url) {
     client.setHeader("Authorization", `Bearer ${token}`);
 
     const variables = {
-      input: { 
-        ...form, 
-        image: imageUrl.url, 
-        createdBy: { 
-          link: creatorId 
-        }
-      }
+      input: {
+        ...form,
+        image: imageUrl.url,
+        createdBy: {
+          link: creatorId,
+        },
+      },
     };
 
     return makeGraphQLRequest(createProjectMutation, variables);
   }
 };
 
-export const updateProject = async (form: ProjectForm, projectId: string, token: string) => {
+export const updateProject = async (
+  form: ProjectForm,
+  projectId: string,
+  token: string
+) => {
   function isBase64DataURL(value: string) {
     const base64Regex = /^data:image\/[a-z]+;base64,/;
     return base64Regex.test(value);
@@ -112,10 +141,10 @@ export const createUser = (name: string, email: string, avatarUrl: string) => {
     input: {
       name: name,
       email: email,
-      avatarUrl: avatarUrl
+      avatarUrl: avatarUrl,
     },
   };
-  
+
   return makeGraphQLRequest(createUserMutation, variables);
 };
 
